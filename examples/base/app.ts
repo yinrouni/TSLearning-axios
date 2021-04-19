e1e484fe55ede8caef5b8274b4445bffb8b82b16
yinrouni <yin.ro@husky.neu.edu>
2021-04-19 20:44:06 +0800

    url params demo

---
+++ examples/base/app.ts
@@ -0,0 +1,63 @@
+// test axios with params in all situations
+import axios from '../../src/ts-axios'
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'bar'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+    bar: 'baz'
+  }
+})
--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,47 +1,47 @@
-import {isDate, isObject} from './utils'
+import { isDate, isObject } from './utils'
 
 
 //对于字符 @、:、$、,、 、[、]，我们是允许出现在 url 中的，不希望被 encode。
-function encode(val: string): string{
+function encode(val: string): string {
   return encodeURIComponent(val).replace(/%40/g, '@')
-  .replace(/%3A/gi, ':')
-  .replace(/%24/g, '$')
-  .replace(/%2C/gi, ',')
-  .replace(/%20/g, '+')
-  .replace(/%5B/gi, '[')
-  .replace(/%5D/gi, ']')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
 }
-export function buildURL(url: string, params?: any): string{
-  if (!params){
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
     return url
   }
 
   const parts: string[] = []
 
-  Object.keys(params).forEach((key)=>{
+  Object.keys(params).forEach((key) => {
     let val = params[key]
-    if (val === null || typeof val === 'undefined'){
-      return 
+    if (val === null || typeof val === 'undefined') {
+      return
     }
 
     let values: string[]
 
     // 参数值为数组， /base/get?foo[]=bar&foo[]=baz
-    if (Array.isArray(val)){
+    if (Array.isArray(val)) {
       values = val
       key = key + '[]'
 
     }
     else {
-      values =[val]
+      values = [val]
     }
 
-    values.forEach((val) =>{
+    values.forEach((val) => {
       ///base/get?date=2019-04-01T05:55:39.030Z，date 后面拼接的是 date.toISOString() 的结果。
-      if (isDate(val)){
+      if (isDate(val)) {
         val = val.toISOString();
       }
-      else if (isObject(val)){
+      else if (isObject(val)) {
         val = JSON.stringify(val)
       }
 
@@ -50,15 +50,15 @@ export function buildURL(url: string, params?: any): string{
   })
 
   let serializedParams = parts.join('&')
-  if (serializedParams){
+  if (serializedParams) {
 
     //丢弃 url 中的哈希标记
     const markIndex = url.indexOf('#')
-    if (markIndex !== -1){
+    if (markIndex !== -1) {
       url = url.slice(0, markIndex)
     }
 
-     url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
   }
 
   return url
