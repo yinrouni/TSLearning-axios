42be2e813e639f7ada4a7d17ebbc096193e000b4
yinrouni <yin.ro@husky.neu.edu>
2021-04-19 20:31:34 +0800

    axios url&params process

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -4,8 +4,21 @@
 
 import {AxiosRequestConfig} from './types/index'
 import xhr from './xhr'
+import {buildURL} from './helpers/url'
+
+function transformUrl(config: AxiosRequestConfig): string{
+  const {url, params} = config
+  return buildURL(url, params)
+}
+
+function processConfig(config: AxiosRequestConfig): void{
+  config.url = transformUrl(config)
+}
+
+
 
 function axios(config: AxiosRequestConfig): void{
+  processConfig(config)
   xhr(config)
 }
 
