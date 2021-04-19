85a1c3664748ccc640659cce3555100c407ad536
yinrouni <yin.ro@husky.neu.edu>
2021-04-19 18:10:48 +0800

    basic rqst

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -1,6 +1,12 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export default class DummyClass {
 
+import {AxiosRequestConfig} from './types/index'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): void{
+  xhr(config)
 }
+
+export default axios
